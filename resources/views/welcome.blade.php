cd012cc5f1373cc1145d87d44477ca3e0f4d3a8d
Pablo Rosa <prosa@link-systems.com>
2017-07-07 16:37:19 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-                <img src="assets/images/Time-Tracking.png">
+                <div class="panel-heading">Welcome To My Time tracking App</div>
+                <div class="text-center">
+                       <img src="assets/images/Time-Tracking.png">
+                </div>
                 <div class="panel-body">
-                <p>Testing my routes</p>
+                <!-- <p>Testing my routes</p> -->
                      <ul class="nav navbar-nav navbar-right">
 <!--                        <li><a href="posts" class="navbar-link">Posts</a></li>
                        <li><a href="allposts" class="navbar-link">All posts</a></li>
