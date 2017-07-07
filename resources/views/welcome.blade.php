bf1c066ad227c58ede044f838e4ae70649a2cf16
Pablo Rosa <prosa@link-systems.com>
2017-07-07 13:22:39 -0400

    updating views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
-
+                <img src="assets/images/Time-Tracking.png">
                 <div class="panel-body">
                 <p>Testing my routes</p>
                      <ul class="nav navbar-nav navbar-right">
-                       <li><a href="posts" class="navbar-link">Posts</a></li>
+<!--                        <li><a href="posts" class="navbar-link">Posts</a></li>
                        <li><a href="allposts" class="navbar-link">All posts</a></li>
                        <li><a href="exercises" class="navbar-link">Exercises</a></li>
                        <li><a href="ex_notes" class="navbar-link">Exercises notes</a></li>
-                       <li><a href="ex_notes" class="navbar-link">USANDO GIT</a></li>
+                       <li><a href="ex_notes" class="navbar-link">USANDO GIT</a></li> -->
          
                    </ul>
                 </div>
