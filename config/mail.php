3dbc2d0cfddc6b91fbe25791137ebe4f94105db9
Pablo <prosa@vxchnge.com>
2018-02-15 12:23:39 -0500

    update to mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'prosa@mail.pablo-rosa.com'),
+        'name' => env('MAIL_FROM_NAME', 'Time Tracker'),
     ],
 
     /*
