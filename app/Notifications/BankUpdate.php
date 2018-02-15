02d30c0df2ccf83fd561175c5e2aa4d572d3659b
Pablo <prosa@vxchnge.com>
2018-02-15 09:22:08 -0500

    should queue

--- app/Notifications/BankUpdate.php
+++ app/Notifications/BankUpdate.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class BankUpdate extends Notification
+class BankUpdate extends Notification implements ShouldQueue
 {
     use Queueable;
 
