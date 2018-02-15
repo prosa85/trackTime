bcfbdb016804f72c55ef450cbaabe7bf485555fe
Pablo <prosa@vxchnge.com>
2018-02-15 12:16:58 -0500

    queue

--- app/Listeners/UpdateToBanckListener.php
+++ app/Listeners/UpdateToBanckListener.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Notification;
 use App\Notifications\BankUpdate;
 
-class UpdateToBanckListener
+class UpdateToBanckListener implements ShouldQueue
 {
     /**
      * Create the event listener.
