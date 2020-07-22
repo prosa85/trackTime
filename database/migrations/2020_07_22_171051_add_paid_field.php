bb83de383e33c4d6acfedc0adbbe28f725b9a707
Pablo_Rosa <pablo.rosa@altruist.com>
2020-07-22 13:17:28 -0400

    adding paid to time track

---
+++ database/migrations/2020_07_22_171051_add_paid_field.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Timetrack;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaidField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('timetracks', function (Blueprint $table) {
+            Schema::table('timetracks', function (Blueprint $table) {
+                $table->boolean('paid')->default(true);
+            });
+
+            $now = Carbon::now();
+            $times = Timetrack::whereYear('created_at', $now->year)->get();
+            $times->each( function ($item){
+                $item->paid = false;
+                $item->save();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('timetracks', function (Blueprint $table) {
+            //
+        });
+    }
+}
